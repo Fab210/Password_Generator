4014ac5fd0edc9a3f3beef56af0df87a43c8005b
Fabio Santos <Fabio.Santos@tns-ilres.com>
2021-10-20 16:43:00 +0200

    first commit

---
+++ Password_Generator/Program.cs
@@ -0,0 +1,71 @@
+﻿using FormationCS;
+using System;
+
+namespace Password_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // NUMBER OFF PASSWORD THAT YOU WANT TO GENERATE
+            const int NB_OF_PASSWORD = 10;
+
+            // VARIABLES
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            string upperLetters = letters.ToUpper();
+            string numbers = "0123456789";
+            string specialchar = "+-*/.";
+            string abc = "";
+
+            // ASK PASSWORD LENGTH
+            int askedNumber = outils.AskNumberBetween("Longueur du mot de passe :", 1, 10);
+
+            // ASK TYPE OF PASSWORD
+            int typeOfPass = outils.AskNumberBetween("Vous voulez un mot de passe avec \n" +
+              "1 - Uniquement des caracters minuscule \n" +
+              "2 - Majuscules et Minuscules \n" +
+              "3 - Caracteres et chiffres \n" +
+              "4 - Caracteres , chiffres et caracteres speciaux \n", 1, 4);
+
+            // SWITCH TYPE OF PASSWORD
+            switch (typeOfPass)
+            {
+                case 1:
+                    abc = letters;
+                    break;
+                case 2:
+                    abc = letters + upperLetters;
+                    break;
+                case 3:
+                    abc = numbers + specialchar;
+                    break;
+                case 4:
+                    abc = letters + upperLetters + numbers + specialchar;
+                    break;
+                default:
+                    break;
+            }
+
+            // COUNT PASSWORD LENGTH
+            int abcLength = abc.Length;
+
+            // INITIALIZE RANDOM INSTANCE
+            Random rand = new Random();
+
+            // GENERATE RANDOM PASSWORDS
+            for (int i = 0; i < NB_OF_PASSWORD + 1; i++)
+            {
+                string password = "";
+                for (int j = 0; j < askedNumber; j++)
+                {
+
+                    int index = rand.Next(0, abcLength);
+                    password = password + abc[index];
+
+
+                }
+                Console.WriteLine("PASSWORD --> " + password);
+            }
+        }
+    }
+}
---
+++ Password_Generator/outils.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int responseInt = 0;
+        //static int askedNumber;
+        public static int AskPositivNumberNotNull(string question)
+        {
+            if (responseInt < 0)
+            {
+                Console.WriteLine("Erreur selectionner un nombre POSITIF");
+                responseInt = AskNumber(question);
+            }
+
+            return responseInt;
+
+        }
+
+        public static int AskNumberBetween(string question, int min, int max)
+        {
+            int askedNumber = AskNumber(question);
+
+
+            while ((askedNumber < min) || (askedNumber > max))
+            {
+                Console.WriteLine("Erreur selectionner un nombre entre " + min + " et " + max);
+                askedNumber = AskNumber(question);
+
+            }
+            return askedNumber;
+        }
+
+        public static int AskNumber(string question)
+        {
+            bool check = true;
+            int notnull = 0;
+
+
+            while (check == true)
+            {
+                Console.Write(question);
+                string res = Console.ReadLine();
+
+                try
+                {
+                    responseInt = int.Parse(res);
+                    notnull = AskPositivNumberNotNull(question);
+
+                    check = false;
+                }
+                catch
+                {
+
+                    Console.WriteLine("Select a number");
+                }
+
+            }
+            return notnull;
+        }
+    }
+}
